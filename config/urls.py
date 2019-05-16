96994388a5fb8a21e02a9b41ffb4227ba7868d77
코딩트리 <codingtree@macbookui-MacBook-Air.local>
2019-05-16 19:27:48 +0900

    기본 페이지 정하기

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('bookmark/', include('bookmark.urls')),
     path('admin/', admin.site.urls),
+    path('', include('bookmark.urls')),
 
 ]
